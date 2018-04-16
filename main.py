4d000fc898222f9f55d55148ad680e3b439a66c9
Joseph Stegall <stlunlimited@gmail.com>
2018-04-16 17:24:08 -0500

    Almost done

---
+++ main.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, redirect, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:build-a-blog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key = 'jfejfkflsk3465grdkfslw3r2'
+
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    blog_title = db.Column(db.String(120))
+    blog_entry = db.Column(db.String(3000))
+    
+
+    def __init__(blog_title, blog_entry):
+        self.blog_entry = blog_entry
+        self.blog_title = blog_title
+        
+    def validation(self):
+        if self.blog_entry and self.blog_title:
+            return True
+        else:
+            return False
+   
+@app.route('/blog', methods=['POST', 'GET']) #displays ALL blog posts
+def display_entries():
+
+    post_id = request.args.get('id')
+    if (post_id):
+        entry = Blog.query.get(post_id)
+        return render_template('singlepost.html', title="Single Blog Entry", entry=entry)
+    
+    else:
+        all_entries = Blog.query.all()
+        return render_template('blog.html', title="All Blog Posts!", all_entries=all_entries)
+
+@app.route('/newpost', methods=['POST', 'GET']) #enter new entries here
+def newpost():
+    if request.method == 'POST':
+        blog_title = request.form['blog_title']
+        post_name = request.form['post']
+        new_entry = Blog(blog_title, post_name)
+
+        db.session.add(new_entry)
+        db.session.commit()
+        url = "/blog?id=" + int(new_entry.id)
+        return redirect(url)
+        
+    else:
+        flash ("Fill out all the fields you slacker!!")
+        return render_template('newpost.html')
+
+    
+
+if __name__ == '__main__':
+    app.run()
